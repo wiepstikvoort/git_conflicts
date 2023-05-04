f0e44cf975ab45f2065fcfb29d6bc67db8ce6fd0
Eider <eider.perezordoyo@upf.edu>
2023-05-04 15:05:45 +0200

    File to work on!
    Please complete from here

---
+++ fibonanci.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+This code is supposed to calculate the Fibonnaci sequence
+and do some other calculations with it
+"""
+
+# FIXME: Refactor the name for this library
+import matplotlib.pyplot as matplotlibpyplotlongnameSebiIhopeyoulikeit
+
+def fibonnaci(number_digits: int) -> list:
+    """Documentation. Please, implement and comment me!"""
+    pass
+
+# Plot something 
+if __name__ == "__main__":
+    # Plot something about the fibonnaci sequence
+    pass
